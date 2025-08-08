00074b571adb0c10433a44a284b31608e1dfc977
asgeev <adamszymanski27@hotmail.com>
2025-08-08 11:03:07 +0200

    Add Prisma integration and guestbook table
    Introduced Prisma for database management with PostgreSQL as the datasource. Created a `Guestbook` model and initialized its migration. Updated the guestbook component to fetch and display data from the database.

--- src/features/guestbook/components/guestbook.tsx
+++ src/features/guestbook/components/guestbook.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
-import { GuestBook } from '@/features/guestbook/libs/types';
-import GuestBookDrawer from '@/features/guestbook/components/guest-book-drawer';
 import AddGuestBookDrawer from '@/features/guestbook/components/add-guest-book-drawer';
+import prisma from '@/lib/prisma';
+import GuestBookDrawer from '@/features/guestbook/components/guest-book-drawer';
+import type { Guestbook } from '@prisma/client';
 
-export default function Guestbook() {
-  const greetings: GuestBook[] = [];
-
+export default async function Guestbook() {
+  const guestbook = await prisma.guestbook.findMany();
+  
   return (
     <>
       <div className='mx-auto max-w-sm px-2 py-10'>
@@ -25,8 +26,15 @@ export default function Guestbook() {
           <p className='font-bold'>Najnowsze:</p>
         </div>
         <div className='mt-8 flex flex-col gap-6'>
-          {greetings.map((g: GuestBook, index: number) => (
-            <GuestBookDrawer key={index} {...g} />
+          {guestbook?.map((g: Guestbook) => (
+            <div>
+              <GuestBookDrawer
+                key={g.id}
+                content={g.content ?? ''}
+                createdAt={g.createdAt}
+                author={g.author}
+              />
+            </div>
           ))}
         </div>
       </div>
